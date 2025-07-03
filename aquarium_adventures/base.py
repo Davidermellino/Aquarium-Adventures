c208dfdcab41d242978832713f72d7115c780b54
dokunoale <153988414+dokunoale@users.noreply.github.com>
2025-07-03 11:56:47 +0200

    feature: adding abse abstract class

--- aquarium_adventures/base.py
+++ aquarium_adventures/base.py
@@ -1,2 +1,13 @@
+from abc import ABC, abstractmethod
+import polars as pl
+
 class BaseAquariumAnalyzer():
-    pass
+    @abstractmethod
+    def analyze_data(self, data: pl.DataFrame) -> pl.DataFrame:
+        """
+        Analyze the aquarium data and return a DataFrame with the results.
+        
+        :param data: A Polars DataFrame containing aquarium data.
+        :return: A Polars DataFrame with the analysis results.
+        """
+        pass
