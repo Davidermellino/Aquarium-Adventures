0bdd8d88d182f19315a3cd0538aafc157129db64
dokunoale <153988414+dokunoale@users.noreply.github.com>
2025-07-03 19:47:56 +0200

    fix: errore di battitura

--- aquarium_adventures/transformations.py
+++ aquarium_adventures/transformations.py
@@ -1,11 +1,8 @@
 import polars as pl
 from aquarium_adventures.base import BaseAquariumAnalyzer
-import polars as pl
-
 
 class AquariumTransformer(BaseAquariumAnalyzer):
     
-
     def __init__(self, tank_info_df_fish_species_split=None):
         pass
     
@@ -20,14 +17,9 @@ class AquariumTransformer(BaseAquariumAnalyzer):
         return pl.DataFrame.join(sensors_df, tank_num_readings_table, on='tank_id') 
 
     def add_avg_ph_per_tank(self, df: pl.DataFrame) -> pl.DataFrame:
-      """ Calcola il pH medio per ogni vasca e lo aggiunge al DataFrame """
+        """ 
+        Calculates the average pH per tank and adds it to the DataFrame 
+        """
 
-      avg_ph = df.group_by("tank_id").agg(
-          pl.col("ph").mean().alias("avg_ph")
-      )
-      return df.join(avg_ph, on="tank_id", how="right")
-
-
-
-    
- 
+        avg_ph = df.group_by("tank_id").agg(pl.col("pH").mean().alias("avg_pH"))
+        return df.join(avg_ph, on="tank_id") 
