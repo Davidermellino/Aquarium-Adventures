14178df84f13f01ad4d9701429866f0153d21a4d
dokunoale <153988414+dokunoale@users.noreply.github.com>
2025-07-03 16:53:12 +0200

    feature: avg_ph_per_tank in transformer class

--- aquarium_adventures/transformations.py
+++ aquarium_adventures/transformations.py
@@ -1,4 +1,12 @@
 from aquarium_adventures.base import BaseAquariumAnalyzer
+import polars as pl
 
 class AquariumTransformer(BaseAquariumAnalyzer):
-    pass
+    
+    def add_avg_ph_per_tank(self, df: pl.DataFrame) -> pl.DataFrame:
+        """ Calcola il pH medio per ogni vasca e lo aggiunge al DataFrame """
+        
+        avg_ph = df.group_by("tank_id").agg(
+            pl.col("ph").mean().alias("avg_ph")
+        )
+        return df.join(avg_ph, on="tank_id", how="right")
